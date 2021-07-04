be33dc9589eefcce4b8af52d8ac46464bbf1f8d8
jardriel sousa <jardriel.redes18@gmail.com>
2021-07-03 23:53:09 -0300

    Revert "first commit"
    This reverts commit c6ecef45f7975c573b7582805a36e33c92be0a41.

--- js/service.js
+++
@@ -1,7 +0,0 @@
-function gerarPdf(){
-    var printContents = document.getElementById('map').innerHTML;
-    var originalContents = document.body.innerHTML;        
-    document.body.innerHTML = printContents;
-    window.print();
-    document.body.innerHTML = originalContents;
-}
